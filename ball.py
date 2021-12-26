309636c0de30f3e55098879588b404b89a45bcc8
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-26 08:24:49 +0530

    final output

--- ball.py
+++ ball.py
@@ -10,8 +10,24 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.penup()
         self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_ball(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((370, 0))
 l_paddle = Paddle((-370, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -22,8 +24,19 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
+    if ball.xcor() > 380:
+        ball.reset_ball()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_ball()
+        scoreboard.r_point()
 screen.exitonclick()
