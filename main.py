b70b60cca289e5d35e8e2b3bd8cde060955e3984
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-26 08:24:49 +0530

    creating left and right paddles

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -9,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((370, 0))
 l_paddle = Paddle((-370, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -18,5 +22,8 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
 screen.exitonclick()
