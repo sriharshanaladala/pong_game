dddec81eca35c358656b6cdf1ca00165d713ec03
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2021-12-26 08:24:49 +0530

    creating left and right paddles

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from turtle import Screen
-
+from paddle import Paddle
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("pong game")
 
+screen.tracer(0)
 
+r_paddle = Paddle((370, 0))
+l_paddle = Paddle((-370, 0))
 
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
 screen.exitonclick()
